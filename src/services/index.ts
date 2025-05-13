c7cf10df4dfd018795e6d8f299364360d102ff85
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2025-05-13 17:23:02 +0300

    update

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,10 @@
+import { user } from './users/users'
 import { bookmarks } from './bookmarks/bookmarks'
 // For more information about this file see https://dove.feathersjs.com/guides/cli/application.html#configure-functions
 import type { Application } from '../declarations'
 
 export const services = (app: Application) => {
+  app.configure(user)
   app.configure(bookmarks)
   // All services will be registered here
 }
